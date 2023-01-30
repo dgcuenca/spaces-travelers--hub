e39916a3a3c09457af1a3ed8e5d3a94b55b86e2e
Shahadat Hossain <120262758+shahadat-pciu@users.noreply.github.com>
2023-01-31 02:40:00 +0600

    Update app.js with routers.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import styles from './App.module.scss';
 import { Route, Routes } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
+import Profile from './pages/Profile';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/my-profile" element={<Profile />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={<Layout />}
+      >
+        <Route
+          index
+          element={<Rockets />}
+        />
+        <Route
+          path="missions"
+          element={<Missions />}
+        />
+        <Route
+          path="my-profile"
+          element={<Profile />}
+        />
+        <Route
+          path="*"
+          element={<NoMatch />}
+        />
+      </Route>
+    </Routes>
   );
 }
 
