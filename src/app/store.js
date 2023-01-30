8d681fb78060a7dc40454cbd9d0ec9ad4e182f64
diego <dgcuenca@gmail.com>
2023-01-30 15:03:57 -0500

    Setup redux logger

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
 const store = configureStore({
   reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 export default store;
