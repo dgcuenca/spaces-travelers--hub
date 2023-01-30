74ca26531eaab8ba2c0a486ab94972f329ebc209
Shahadat Hossain <120262758+shahadat-pciu@users.noreply.github.com>
2023-01-31 02:40:33 +0600

    Fix linter error in index.js file.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './app/store';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
 
 const container = document.getElementById('root');
 const root = createRoot(container);
