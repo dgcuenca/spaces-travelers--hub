d43e13e9256c14f046b752699135fa8d34d11f37
diego <dgcuenca@gmail.com>
2023-01-30 17:18:34 -0500

    Style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import styles from './Header.module.scss';
 
-const Header = () => <div>Our Header</div>;
+const Header = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: './missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: './my-profile',
+      text: 'My Profile',
+    },
+  ];
+
+  return (
+    <nav className={styles.navBar}>
+      <Link to="/">
+        <h1>
+          <img src="/asses/planet.png" alt="logo" className={styles.logo} />
+          Space Traveler&#39;s Hub
+        </h1>
+      </Link>
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path}>
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Header;
