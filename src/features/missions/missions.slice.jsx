300f04feac981b50a404f8fa061781c7a6af8818
diego <dgcuenca@gmail.com>
2023-01-31 18:35:04 -0500

    Set up reducer

--- src/features/missions/missions.slice.jsx
+++ src/features/missions/missions.slice.jsx
@@ -1,18 +1,50 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const MISSIONS_URL = 'https://api.spacexdata.com/v3/missions';
+// Actions
+const FETCH_MISSIONS = 'missions/FETCH_MISSIONS';
 
-export const fetchMissions = createAsyncThunk(
-  'missions/FETCH_MISSIONS',
-  async () => {
-    const response = await axios.get(MISSIONS_URL);
-    const missions = response.data;
-    const modifiedMissions = missions.map((mission) => ({
-      mission_id: mission.mission_id,
-      mission_name: mission.mission_name,
-      description: mission.description,
-    }));
-    return modifiedMissions;
-  },
+// Action creators
+export const fetchMissions = createAsyncThunk(FETCH_MISSIONS, async () => {
+  const response = await axios.get(MISSIONS_URL);
+  const missions = response.data;
+  const modifiedMissions = missions.map((mission) => ({
+    mission_id: mission.mission_id,
+    mission_name: mission.mission_name,
+    description: mission.description,
+  }));
+  return modifiedMissions;
+},
 );
+
+// Reducer
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState: {
+    missions: [],
+    loading: false,
+    error: null,
+  },
+  extraReducers: {
+    [fetchMissions.pending]: (state) => {
+      const newState = { ...state, loading: true };
+    },
+    [fetchMissions.fulfilled]: (state, action) => {
+      const newState = {
+        ...state,
+        loading: false,
+        missions: action.payload,
+      };
+    },
+    [fetchMissions.rejected]: (state) => {
+      const newState = {
+        ...state,
+        loading: false,
+        error: action.error.message,
+      };
+    },
+  },
+});
+
+export default missionsSlice.reducer;
