0f2826d8c960914b5234ee3c9e44677b40e32118
diego <dgcuenca@gmail.com>
2023-02-01 18:58:06 -0500

    Add functionality to join mission button

--- src/components/missions/Mission.jsx
+++ src/components/missions/Mission.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import styles from './Mission.module.scss';
+import { joinMission } from '../../features/missions/missions.slice';
 
-const Mission = ({ missions }) => (
-  missions.map((mission) => (
-    <li key={mission.mission_id} className={styles.mission}>
-      <p className={styles.name}>{mission.mission_name}</p>
-      <p className={styles.description}>{mission.description}</p>
-      <div className={styles.status}>
-        <button
-          type="button"
-        >
-          NOT A MEMBER
-        </button>
-      </div>
-      <div className={styles.empty}>
-        <button
-          type="button"
-        >
-          Join Mission
-        </button>
-      </div>
-    </li>
-  ))
-);
+const Mission = ({ missions }) => {
+  const dispatch = useDispatch();
+
+  function joinHandler(id) {
+    dispatch(joinMission(id));
+  }
+
+  return (
+    missions.map((mission) => (
+      <li key={mission.mission_id} className={styles.mission}>
+        <p className={styles.name}>{mission.mission_name}</p>
+        <p className={styles.description}>{mission.description}</p>
+        <div className={styles.status}>
+          <button
+            type="button"
+          >
+            NOT A MEMBER
+          </button>
+        </div>
+        <div className={styles.empty}>
+          <button
+            type="button"
+            onClick={() => joinHandler(mission.mission_id)}
+          >
+            Join Mission
+          </button>
+        </div>
+      </li>
+    ))
+  );
+};
 
 Mission.propTypes = {
   missions: PropTypes.arrayOf(PropTypes.shape({
