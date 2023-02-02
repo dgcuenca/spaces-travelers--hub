5e3e5a37261956c24d7af9b47bcf4a70dd8b2eec
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-02 19:45:21 +0600

    Show reserved rocket dynamically.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { reservedRockets } from '../features/rockets/rockets.slice';
 import styles from './Profile.module.scss';
 
-const Profile = () => (
-  <div className={styles.profile}>
-    <div className={styles.profile__wrapper}>
-      <h2 className={styles.profile__title}>My Missions</h2>
-      <ul className={styles.profile__items}>
-        <li className={styles.profile__item}>Falcon9</li>
-        <li className={styles.profile__item}>Falcon Heavy</li>
-        <li className={styles.profile__item}>Starship</li>
-      </ul>
+const Profile = () => {
+  const rockets = useSelector(reservedRockets);
+  return (
+    <div className={styles.profile}>
+      <div className={styles.profile__wrapper}>
+        <h2 className={styles.profile__title}>My Missions</h2>
+        <ul className={styles.profile__items}>
+          <li className={styles.profile__item}>Falcon9</li>
+          <li className={styles.profile__item}>Falcon Heavy</li>
+          <li className={styles.profile__item}>Starship</li>
+        </ul>
+      </div>
+      <div className={styles.profile__wrapper}>
+        <h2 className={styles.profile__title}>My Rockets</h2>
+        <ul className={styles.profile__items}>
+          {rockets.map((rocket) => (
+            <li
+              className={styles.profile__item}
+              key={rocket.id}
+            >
+              {rocket.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <div className={styles.profile__wrapper}>
-      <h2 className={styles.profile__title}>My Rockets</h2>
-      <ul className={styles.profile__items}>
-        <li className={styles.profile__item}>Falcon9</li>
-        <li className={styles.profile__item}>Falcon Heavy</li>
-        <li className={styles.profile__item}>Starship</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Profile;
