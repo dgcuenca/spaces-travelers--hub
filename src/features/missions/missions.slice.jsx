d7900f0d33109de6a22f2b7ddef3a4e629a1a4d0
diego <dgcuenca@gmail.com>
2023-02-01 19:49:46 -0500

    Update state when user click

--- src/features/missions/missions.slice.jsx
+++ src/features/missions/missions.slice.jsx
@@ -25,6 +25,15 @@ const missionsSlice = createSlice({
     loading: 'idle',
     error: null,
   },
+  reducers: {
+    joinMission: (state, action) => {
+      const newState = state.missions.map((mission) => {
+        if (mission.id !== action.payload) return mission;
+        return { ...mission, reserved: true };
+      });
+      return newState;
+    },
+  },
   extraReducers: {
     [fetchMissions.pending]: (state) => {
       const newState = { ...state, loading: 'loading' };
@@ -48,5 +57,7 @@ const missionsSlice = createSlice({
     },
   },
 });
+
+export const { joinMission } = missionsSlice.actions.joinMission;
 
 export default missionsSlice.reducer;
