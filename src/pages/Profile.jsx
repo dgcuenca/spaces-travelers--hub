eb2db93b59e4f128c23a34edab50c04bb0fe683f
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-02 19:43:40 +0600

    Add profile skeleton in Profile page.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import styles from './Profile.module.scss';
 
-const Profile = () => <div>Profile</div>;
+const Profile = () => (
+  <div className={styles.profile}>
+    <div className={styles.profile__wrapper}>
+      <h2 className={styles.profile__title}>My Missions</h2>
+      <ul className={styles.profile__items}>
+        <li className={styles.profile__item}>Falcon9</li>
+        <li className={styles.profile__item}>Falcon Heavy</li>
+        <li className={styles.profile__item}>Starship</li>
+      </ul>
+    </div>
+    <div className={styles.profile__wrapper}>
+      <h2 className={styles.profile__title}>My Rockets</h2>
+      <ul className={styles.profile__items}>
+        <li className={styles.profile__item}>Falcon9</li>
+        <li className={styles.profile__item}>Falcon Heavy</li>
+        <li className={styles.profile__item}>Starship</li>
+      </ul>
+    </div>
+  </div>
+);
 
 export default Profile;
