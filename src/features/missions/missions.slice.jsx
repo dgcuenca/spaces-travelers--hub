cde88b993416561fafe97ef831c804dab4e7a85b
diego <dgcuenca@gmail.com>
2023-02-02 12:28:56 -0500

    Get joined missions

--- src/features/missions/missions.slice.jsx
+++ src/features/missions/missions.slice.jsx
@@ -17,6 +17,13 @@ export const fetchMissions = createAsyncThunk(FETCH_MISSIONS, async () => {
   return modifiedMissions;
 });
 
+export const getJoinedMissions = (state) => {
+  const joinedMissions = state.missions.filter(
+    (mission) => mission.reserved === true,
+  );
+  return joinedMissions;
+};
+
 // Reducer
 const missionsSlice = createSlice({
   name: 'missions',
