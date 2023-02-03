29a1b8be7b13cc756f7916e0a028b1dc01aa4f37
diego <dgcuenca@gmail.com>
2023-02-03 11:25:54 -0500

    Test correctly render of mission component

--- src/components/missions/Mission.test.js
+++ src/components/missions/Mission.test.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import Mission from './Mission';
 
 test('renders mission', () => {
-  const mission = {
-    mission_id: '1',
-    mission_name: 'Apollo13',
-    description: 'mission',
-    reserved: true
-  }
-  
-  const component = render (<Mission mission={mission} />);
+  const missions = [
+    {
+      mission_id: '1',
+      mission_name: 'Apollo13',
+      description: 'mission',
+      reserved: true
+    }
+  ]
+  const mockStore = configureStore();
+  let store=mockStore(missions)
 
-  component.getByText('Active Member')
+  const component = render(<Provider store={store}><Mission missions={missions} /></Provider>);
+
+  component.getByText('Active Member');//since reserved is passing as true
+  expect(component.container).toHaveTextContent(missions[0].mission_name);
+  
+})
+
+test('Clicking join Button', () => {
+  const missions = [
+    {
+      mission_id: '1',
+      mission_name: 'Apollo13',
+      description: 'mission',
+      reserved: true
+    }
+  ]
+  const mockStore = configureStore();
+  let store=mockStore(missions)
+
+  const component = render(<Provider store={store}><Mission missions={missions} /></Provider>);
+  const button = component.getByText('Active Member');
+
+  
 })
