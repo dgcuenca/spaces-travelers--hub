8098e7dbeb0326a6df0468c81c3c1c0f9089e000
diego <dgcuenca@gmail.com>
2023-02-03 12:36:31 -0500

    Add test for badges

--- src/components/missions/Mission.test.js
+++ src/components/missions/Mission.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 import '@testing-library/jest-dom/extend-expect';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Mission from './Mission';
 
 test('renders mission', () => {
@@ -15,29 +15,29 @@ test('renders mission', () => {
     }
   ]
   const mockStore = configureStore();
-  let store=mockStore(missions)
-
+  let store = mockStore(missions)
   const component = render(<Provider store={store}><Mission missions={missions} /></Provider>);
-
-  component.getByText('Active Member');//since reserved is passing as true
+  ;//since reserved is passing as true
   expect(component.container).toHaveTextContent(missions[0].mission_name);
-  
+
 })
 
-test('Clicking join Button', () => {
+test('renders bagets', () => {
   const missions = [
     {
       mission_id: '1',
       mission_name: 'Apollo13',
       description: 'mission',
-      reserved: true
+      reserved: false
     }
   ]
   const mockStore = configureStore();
-  let store=mockStore(missions)
+  let store = mockStore(missions)
 
   const component = render(<Provider store={store}><Mission missions={missions} /></Provider>);
-  const button = component.getByText('Active Member');
 
-  
+  ;//since reserved is passing as false
+  expect(component.container).toHaveTextContent('NOT A MEMBER');
+  expect(component.container).toHaveTextContent('Join Mission');
+
 })
