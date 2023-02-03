b96303985723f9190b827e801a751a34ff84424a
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-03 17:03:15 +0600

    Fix linter errors.

--- src/pages/__test__/Profile.test.js
+++ src/pages/__test__/Profile.test.js
@@ -5,7 +5,6 @@
 import Profile from '../Profile';
 import '@testing-library/jest-dom';
 import renderWithProviders from '../../../testingHelper';
-import store from '../../app/store';
 
 describe('Profile Page', () => {
   test('renders Profile Page', () => {
