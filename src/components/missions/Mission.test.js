a8088d731cb8111b7523fb775c637f0b2e27f1ec
diego <dgcuenca@gmail.com>
2023-02-03 13:44:56 -0500

    Fix linters errors

--- src/components/missions/Mission.test.js
+++ src/components/missions/Mission.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import '@testing-library/jest-dom/extend-expect';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Mission from './Mission';
 
 test('renders mission', () => {
@@ -11,16 +11,15 @@ test('renders mission', () => {
       mission_id: '1',
       mission_name: 'Apollo13',
       description: 'mission',
-      reserved: true
-    }
-  ]
+      reserved: true,
+    },
+  ];
   const mockStore = configureStore();
-  let store = mockStore(missions)
+  const store = mockStore(missions);
   const component = render(<Provider store={store}><Mission missions={missions} /></Provider>);
-  ;//since reserved is passing as true
+  // since reserved is passing as true
   expect(component.container).toHaveTextContent(missions[0].mission_name);
-
-})
+});
 
 test('renders bagets', () => {
   const missions = [
@@ -28,16 +27,15 @@ test('renders bagets', () => {
       mission_id: '1',
       mission_name: 'Apollo13',
       description: 'mission',
-      reserved: false
-    }
-  ]
+      reserved: false,
+    },
+  ];
   const mockStore = configureStore();
-  let store = mockStore(missions)
+  const store = mockStore(missions);
 
   const component = render(<Provider store={store}><Mission missions={missions} /></Provider>);
 
-  ;//since reserved is passing as false
+  // since reserved is passing as false
   expect(component.container).toHaveTextContent('NOT A MEMBER');
   expect(component.container).toHaveTextContent('Join Mission');
-
-})
+});
